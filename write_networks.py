9115921e3797e140d226913827715a82cb18c2ad
mcmurryj <jonathan.mcmurry@gmail.com>
2017-02-11 00:00:59 -0800

    fix thing with float v. string in network writing branch.

--- write_networks.py
+++ write_networks.py
@@ -10,7 +10,7 @@ def write_networks(data_box, g_output_dir) :
 	for cluster_ID in data_box.keys():
 		for cluster_member_ID in data_box[cluster_ID].keys():
 			for homologue_ID in data_box[cluster_ID][cluster_member_ID].keys():
-				score               = data_box[cluster_ID][cluster_member_ID][homologue_ID]["bitscore"]
+				score               = float(data_box[cluster_ID][cluster_member_ID][homologue_ID]["bitscore"])
 				homologue_parent_ID = data_box[cluster_ID][cluster_member_ID][homologue_ID]["homologue_parent"]
 				IDtup               = tuple(sorted([cluster_ID, homologue_parent_ID]))
 				IDtup2              = tuple(sorted([cluster_member_ID, homologue_ID]))
