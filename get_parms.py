9858c47c533551f4a6547f6a683dbf7015f0cbc6
mcmurryj <jonathan.mcmurry@gmail.com>
2017-02-17 23:58:52 -0800

    Added functionality to system for retrieving input parameters.

--- get_parms.py
+++ get_parms.py
@@ -3,17 +3,30 @@
 import argparse
 def get_parms():
     parser = argparse.ArgumentParser()
-    parser.add_argument("input", help = "your input protein sequence in fasta format")
-    parser.add_argument("database", help = "your input BLAST database")
-    parser.add_argument("output", help = "the name of the directory where you wish to store output")
+    parser.add_argument("-input",
+                        help = "your input protein sequence in fasta format")
+    parser.add_argument("-database",
+                        help = "your input BLAST database")
+    parser.add_argument("-output",
+                        help = "the name of the directory where you wish to store output")
+    parser.add_argument("--query_eval",
+                       default = 1E-30,
+                        help = "The e value cutoff to use for the initial blast search")
+    parser.add_argument("--feature_radius",
+                         default = 5
+                         help = "How wide a window around your initial hit do you want to consider, in coding sequences?")
+    parser.add_argument("--handshake_eval",
+                        default = 1E-20,
+                        help = "E value cutoff for second round of BLAST.")
+
     args = parser.parse_args()
     return ({
     "query_fa"           : args.input,
     "output_dir"         : args.output,
-    "query_eval"         : 1E-30,				#cutoff evalue for the initial BLAST search
-    "feature_radius"     : 5,		#how many proteins on either side to take for subsequent analysis?
+    "query_eval"         : args.query_eval,				#cutoff evalue for the initial BLAST search
+    "feature_radius"     : args.feature_radius,		 #how many proteins on either side to take for subsequent analysis?
     "max_no_seqs"        : 200,				#maximum number of hits to return in the initial search
     "handshake_eval"	 : 1e-20,			#cutoff evalue for the _blast_seq blast searches
-    "blastdb"            : args.database,					#use nr as the db for initial blast search by default
+    "blastdb"            : args.database,
     })
     print("Done setting inital variables.  Input sequence from: " + query_fa + "; Output goes in " + output_dir)
